b782ce391e5d2e2ae13b46ced835b21fa86a2215
manikandanaitech <manikandan.s@aitechindia.com>
2019-08-30 10:53:32 +0530

    size class

---
+++ sizeClasses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sizeClasses
+//
+//  Created by Anil Kumar on 16/08/19.
+//  Copyright © 2019 AIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
